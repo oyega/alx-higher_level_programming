58216ebf159a8372fa48f7f6f0e9d21f19055495
oyega <oyega02@gmail.com>
2022-09-20 21:46:25 +0100

    0-positive_or_negative.py is a program will assign a random signed number to the variable number each time it is executed. Complete the source code in order to print whether the number stored in the variable number is positive or negative.

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
+print('{} is '.format(number), end='')
 if number > 0:
-print("{} is positive".format(number))
+    print('positive')
 elif number == 0:
-print("{} is zero".format(number))
+    print('zero')
 else:
-print("{} is negative".format(number))
+    print('negative')
