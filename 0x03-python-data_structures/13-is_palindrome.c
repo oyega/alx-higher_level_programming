969dcc5102f20d1c5b2a06ac2ba21cb815ec6c45
oyega <oyega02@gmail.com>
2022-09-25 22:12:38 +0100

    13-is_palindrome.c is a function in C that checks if a singly linked list is a palindrome.

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,114 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * reverse - reverses the second half of the list
+ * add_nodeint - adds a new node at the start of a listint_t list
+ * @head: pointer to pointer of first node of listint_t list
+ * @n: integer to be included in new node
  *
- * @h_r: head of the second half
- * Return: no return
+ * Return: address of the new element or NULL if it fails
  */
-void reverse(listint_t **h_r)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *prv;
-	listint_t *crr;
-	listint_t *nxt;
+	listint_t *new;
 
-	prv = NULL;
-	crr = *h_r;
+	if (!head)
+		return (NULL);
 
-	while (crr != NULL)
-	{
-		nxt = crr->next;
-		crr->next = prv;
-		prv = crr;
-		crr = nxt;
-	}
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 
-	*h_r = prv;
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
 }
 
 /**
- * compare - compares each int of the list
- *
- * @h1: head of the first half
- * @h2: head of the second half
- * Return: 1 if are equals, 0 if not
- */
-int compare(listint_t *h1, listint_t *h2)
-{
-	listint_t *tmp1;
-	listint_t *tmp2;
-
-	tmp1 = h1;
-	tmp2 = h2;
-
-	while (tmp1 != NULL && tmp2 != NULL)
-	{
-		if (tmp1->n == tmp2->n)
-		{
-			tmp1 = tmp1->next;
-			tmp2 = tmp2->next;
-		}
-		else
-		{
-			return (0);
-		}
-	}
-
-	if (tmp1 == NULL && tmp2 == NULL)
-	{
-		return (1);
-	}
-
-	return (0);
-}
-
-/**
- * is_palindrome - checks if a singly linked list
- * is a palindrome
- * @head: pointer to head of list
- * Return: 0 if it is not a palindrome,
- * 1 if it is a palndrome
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: duoble pointer to start of singly linked list
+ * 
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *slow, *fast, *prev_slow;
-	listint_t *scn_half, *middle;
-	int isp;
+	listint_t *temp = *head;
+	listint_t *b_half = NULL, *b_half_t = NULL;
+	unsigned int len, i;
 
-	slow = fast = prev_slow = *head;
-	middle = NULL;
-	isp = 1;
-
-	if (*head != NULL && (*head)->next != NULL)
+	if (!*head)
+		return (1);
+/* measure list */
+	for (len = 0; temp; len++)
 	{
-		while (fast != NULL && fast->next != NULL)
-		{
-			fast = fast->next->next;
-			prev_slow = slow;
-			slow = slow->next;
-		}
+		temp = temp->next;
+	}
+	temp = *head;
 
-		if (fast != NULL)
+/* copy second half */
+	for (i = 0; i < len; i++)
+	{
+		if (i >= (len - (len / 2)))
 		{
-			middle = slow;
-			slow = slow->next;
+			add_nodeint(&b_half, temp->n);
 		}
+		temp = temp->next;
+	}
+	temp = *head;
+	b_half_t = b_half;
 
-		scn_half = slow;
-		prev_slow->next = NULL;
-		reverse(&scn_half);
-		isp = compare(*head, scn_half);
-
-		if (middle != NULL)
+/* compare first half and reversed second half */
+	for (i = 0; i < (len / 2); i++)
+	{
+		if (temp->n != b_half->n)
 		{
-			prev_slow->next = middle;
-			middle->next = scn_half;
+			free_listint(b_half_t);
+			return (0);
 		}
-		else
-		{
-			prev_slow->next = scn_half;
-		}
+		temp = temp->next;
+		b_half = b_half->next;
 	}
 
-	return (isp);
+/* free second half */
+	free_listint(b_half_t);
+	return (1);
 }
