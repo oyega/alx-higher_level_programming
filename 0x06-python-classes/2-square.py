bd7c9835c00157c448445e6e06e9f75df7ceb373
oyega <oyega02@gmail.com>
2022-10-12 00:07:27 +0100

    2-square.py is a class Square that defines a square by: (based on 1-square.py)

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This is module 2-square
+This module contains one class Square
+"""
+
+
+class Square:
+    """
+    This class Square does not do anything
+    **parameters**
+    size: needed for instantiation, hidden (should it be mentioned here ?)
+    **functions**
+    __init__(self, size)
+    :Example:
+    x = Square(3)
+    """
+
+    def __init__(self, size=0):
+        """
+        instantiate a Square object
+        Arguments:
+            size: must be a positive or null integer
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
