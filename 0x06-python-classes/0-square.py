31687faddcf6aae141fcc912a961198fcb611b7b
oyega <oyega02@gmail.com>
2022-10-12 00:00:55 +0100

    0-square.py is a an empty class Square that defines a square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is module 0-square
+It contains one very basic class Square
+"""
+
+
+class Square:
+    """
+    This class Square is empty
+    :Example:
+    x = Square()
+    """
+    pass
